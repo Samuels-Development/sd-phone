f14fd2ca73924ecfc22b5578e6027c34074bbd5a
Samuel Nicol <99494967+Github-Samuel@users.noreply.github.com>
2026-07-19 15:16:33 +0200

    chore(cherry): drop committed web/build bundle (shipped via release now)

--- web/build/assets/config-DtR7kERl.js
+++
@@ -1,1 +0,0 @@
-import{Q as a,R as p}from"./index-D0GEFK6M.js";let e=null;async function o(){return e||(a?(e={people:(await p("sd-phone:maps:config"))?.people!==!1},e):(e={people:!0},e))}export{o as m};
